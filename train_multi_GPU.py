efd86b5d2ba04a1fce19f296bbfd14b3ee242215
chuanbosi <chuanbosi001@gmail.com>
2023-09-10 15:19:35 +0800

    Uploading anntotion for train_mutil_GPU.py

--- train_multi_GPU.py
+++ train_multi_GPU.py
@@ -10,7 +10,7 @@ from my_dataset import VOCSegmentation
 import transforms as T
 
 
-class SegmentationPresetTrain:
+class SegmentationPresetTrain:  # 训练数据预处理,和之前的模型一样图像数据增广处理
     def __init__(self, base_size, crop_size, hflip_prob=0.5, mean=(0.485, 0.456, 0.406), std=(0.229, 0.224, 0.225)):
         min_size = int(0.5 * base_size)
         max_size = int(2.0 * base_size)
@@ -49,14 +49,14 @@ def get_transform(train):
 
 
 def create_model(aux, num_classes):
-    model = deeplabv3_resnet50(aux=aux, num_classes=num_classes)
-    weights_dict = torch.load("./deeplabv3_resnet50_coco.pth", map_location='cpu')
+    model = deeplabv3_resnet50(aux=aux, num_classes=num_classes)  # 使用resnet50作为我们backbone
+    weights_dict = torch.load("./deeplabv3_resnet50_coco.pth", map_location='cpu')  #导入预训练权重
 
     if num_classes != 21:
         # 官方提供的预训练权重是21类(包括背景)
         # 如果训练自己的数据集，将和类别相关的权重删除，防止权重shape不一致报错
         for k in list(weights_dict.keys()):
-            if "classifier.4" in k:
+            if "classifier.4" in k:    # 会删去分类器的最后一个卷积的通道数
                 del weights_dict[k]
 
     missing_keys, unexpected_keys = model.load_state_dict(weights_dict, strict=False)
@@ -68,7 +68,7 @@ def create_model(aux, num_classes):
 
 
 def main(args):
-    init_distributed_mode(args)
+    init_distributed_mode(args)  # 启动多进程
     print(args)
 
     device = torch.device(args.device)
@@ -78,7 +78,7 @@ def main(args):
     # 用来保存coco_info的文件
     results_file = "results{}.txt".format(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
 
-    VOC_root = args.data_path
+    VOC_root = args.data_path  # 导入我们的数据集的路径
     # check voc root
     if os.path.exists(os.path.join(VOC_root, "VOCdevkit")) is False:
         raise FileNotFoundError("VOCdevkit dose not in path:'{}'.".format(VOC_root))
@@ -88,18 +88,18 @@ def main(args):
     train_dataset = VOCSegmentation(args.data_path,
                                     year="2012",
                                     transforms=get_transform(train=True),
-                                    txt_name="train.txt")
+                                    txt_name="train.txt")  #导入数据集
     # load validation data set
     # VOCdevkit -> VOC2012 -> ImageSets -> Segmentation -> val.txt
     val_dataset = VOCSegmentation(args.data_path,
                                   year="2012",
                                   transforms=get_transform(train=False),
-                                  txt_name="val.txt")
+                                  txt_name="val.txt") #导入验证集
 
     print("Creating data loaders")
     if args.distributed:
         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-        test_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset)
+        test_sampler = torch.utils.data.distributed.DistributedSampler(val_dataset) # 这里我们直接将验证集作为了测试集，并且启动了多进程模式
     else:
         train_sampler = torch.utils.data.RandomSampler(train_dataset)
         test_sampler = torch.utils.data.SequentialSampler(val_dataset)
@@ -107,20 +107,20 @@ def main(args):
     train_data_loader = torch.utils.data.DataLoader(
         train_dataset, batch_size=args.batch_size,
         sampler=train_sampler, num_workers=args.workers,
-        collate_fn=train_dataset.collate_fn, drop_last=True)
+        collate_fn=train_dataset.collate_fn, drop_last=True)  # 创建训练的dataloader,将drop_last=True后，在训练的时候，最后一个batch不足一个batch时，会将其丢弃
 
     val_data_loader = torch.utils.data.DataLoader(
         val_dataset, batch_size=1,
         sampler=test_sampler, num_workers=args.workers,
-        collate_fn=train_dataset.collate_fn)
+        collate_fn=train_dataset.collate_fn)  # 创建验证的dataloader
 
     print("Creating model")
     # create model num_classes equal background + 20 classes
-    model = create_model(aux=args.aux, num_classes=num_classes)
+    model = create_model(aux=args.aux, num_classes=num_classes)  # 调用create_model函数，并传入参数
     model.to(device)
 
     if args.sync_bn:
-        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
+        model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)  # 是不是使用多进程的BN处理
 
     model_without_ddp = model
     if args.distributed:
@@ -130,17 +130,17 @@ def main(args):
     params_to_optimize = [
         {"params": [p for p in model_without_ddp.backbone.parameters() if p.requires_grad]},
         {"params": [p for p in model_without_ddp.classifier.parameters() if p.requires_grad]},
-    ]
+    ]  # 这个模型的backbone和分类器部分进行参数优化
     if args.aux:
         params = [p for p in model_without_ddp.aux_classifier.parameters() if p.requires_grad]
-        params_to_optimize.append({"params": params, "lr": args.lr * 10})
+        params_to_optimize.append({"params": params, "lr": args.lr * 10})  # 对于使用辅助分类器的操作，进行参数优化，设置优化器，学习率
     optimizer = torch.optim.SGD(
         params_to_optimize,
-        lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)
+        lr=args.lr, momentum=args.momentum, weight_decay=args.weight_decay)  #使用SGD作为优化器，以及一些参数设定，像动量、权重衰减、学习率
 
-    scaler = torch.cuda.amp.GradScaler() if args.amp else None
+    scaler = torch.cuda.amp.GradScaler() if args.amp else None  # 使用自动混合精度作为我们的梯度更新策略
 
-    # 创建学习率更新策略，这里是每个step更新一次(不是每个epoch)
+    # 创建学习率更新策略，这里是每个step更新一次(不是每个epoch)，也就是一个batch更新一次
     lr_scheduler = create_lr_scheduler(optimizer, len(train_data_loader), args.epochs, warmup=True)
 
     # 如果传入resume参数，即上次训练的权重地址，则接着上次的参数训练
@@ -156,26 +156,27 @@ def main(args):
         if args.amp:
             scaler.load_state_dict(checkpoint["scaler"])
 
-    if args.test_only:
+    if args.test_only:  # 测试要用到的模型，数据集
         confmat = evaluate(model, val_data_loader, device=device, num_classes=num_classes)
         val_info = str(confmat)
         print(val_info)
         return
 
     print("Start training")
-    start_time = time.time()
-    for epoch in range(args.start_epoch, args.epochs):
-        if args.distributed:
+    start_time = time.time()  # 记录时间
+    for epoch in range(args.start_epoch, args.epochs):  #开始循环
+        if args.distributed:  # 是否启用多进程训练
             train_sampler.set_epoch(epoch)
+            #从一个epoch中获取学习率和损失，这个train_one_epoch是导入官方的模块，传进去模型，优化器，dataloader等等
         mean_loss, lr = train_one_epoch(model, optimizer, train_data_loader, device, epoch,
                                         lr_scheduler=lr_scheduler, print_freq=args.print_freq, scaler=scaler)
 
-        confmat = evaluate(model, val_data_loader, device=device, num_classes=num_classes)
-        val_info = str(confmat)
+        confmat = evaluate(model, val_data_loader, device=device, num_classes=num_classes) #，验证部分如此
+        val_info = str(confmat) #将验证的部分转化成字符串的形式，打印下来
         print(val_info)
 
         # 只在主进程上进行写操作
-        if args.rank in [-1, 0]:
+        if args.rank in [-1, 0]:  # 针对这一行代码，rank为0，代表的是主进程，而rank为-1时是特殊的角色，通常不承担任务或计算，更多的用于
             # write into txt
             with open(results_file, "a") as f:
                 # 记录每个epoch对应的train_loss、lr以及验证集各指标
@@ -196,7 +197,7 @@ def main(args):
             save_on_master(save_file,
                            os.path.join(args.output_dir, 'model_{}.pth'.format(epoch)))
 
-    total_time = time.time() - start_time
+    total_time = time.time() - start_time # 计算总时间
     total_time_str = str(datetime.timedelta(seconds=int(total_time)))
     print('Training time {}'.format(total_time_str))
 
